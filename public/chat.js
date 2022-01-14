a9559832a9c1ff019af5b823b44ba07be7b36e77
unknown <user@DESKTOP-6D9BA82.soldasul.local>
2022-01-14 17:54:31 -0300

    add typing check and showing to user

--- public/chat.js
+++ public/chat.js
@@ -1,27 +1,35 @@
 
 const socket = io(window.location.href);
 
-let user = '';
+let user = 'Tudo446';
 let userCheck = false;
 
 socket.on('update_messages', (messages) => {
 
     updateMessages(messages);
-
 })
+
+socket.on('user-typing', (data) => {
+    showUserTyping(data);
+})
+
+
+
 
 function updateMessages(data){
     let div_message = document.querySelector('#messages');
 
-    for(let i=div_message.childElementCount; i > 0; i--){
-        div_message.removeChild(div_message.children[0]);
-    }
+    
+    // for(let i=div_message.childElementCount; i > 0; i--){
+    //     div_message.removeChild(div_message.children[0]);
+    // }
 
-    data.forEach( contentBodyMSG => {
-        let node = composeMsg(contentBodyMSG);
+    data.forEach( data => {
+        let node = composeMsg(data);
         div_message.appendChild(node);
     });
 
+    // add a margin-top on first message
     if(div_message.childElementCount > 0){
         div_message.children[0].classList.add('first-container-top');
     }   
@@ -36,8 +44,7 @@ function composeMsg(data){
         
 
     if(data.user == user) {
-
-        
+  
         if(!userCheck){
             msgContainer.classList.add('first-container');
             userCheck = true;
@@ -70,8 +77,54 @@ function composeMsg(data){
     return msgContainer;
 }
 
+function showUserTyping(user) {
+
+    console.log(user);
+    
+    let userContent = document.querySelector('#users-typing');
+    
+    let usersTyping = '';
+
+    
+        
+    usersTyping += `${user.userTyping},`;
+    
+
+    usersTyping += ' is typing...';
+
+    userContent.appendChild(document.createTextNode(usersTyping));
+    userContent.classList.add('active');
+
+    let timeShowingUser = setTimeout(() => {
+        userContent.classList.remove('active');
+        userContent.innerHTML = '';
+    },3000);
+
+    console.log(usersTyping);
+}
+
+
 document.addEventListener('DOMContentLoaded', ()=> {
 
+    let inputTimeSender;
+    let timeStarted = false;
+
+    document.forms['message_form_name']['msg'].addEventListener('input', () => {
+        
+        if(!inputTimeSender && !timeStarted ) {
+
+            socket.emit('typing', { user: user } );
+            console.log('user is typing...');
+
+
+            timeStarted = true;
+
+            let inputTimeSender = setTimeout(()=>{
+                clearInterval(inputTimeSender);
+                timeStarted = false;
+            },2750);
+        }
+    })
 
     const form = document.querySelector('#message_form');
 
@@ -88,6 +141,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
         socket.emit('new_message', { user: user, msg: message });
         console.log(message);
     })
+
+
 
     const userForm = document.querySelector('#user_form');
     userForm.addEventListener('submit', (e) => {
